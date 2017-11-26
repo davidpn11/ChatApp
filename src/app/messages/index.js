b3e3a16a1868d888c127e5672db62e485f1bb474
David Nascimento <davidpn.11@gmail.com>
2017-11-26 18:08:55 -0200

    added scroll glue

--- src/app/messages/index.js
+++ src/app/messages/index.js
@@ -4,8 +4,9 @@ import messageBox from './messageBox/messagebox.component'
 import messageSender from './messageSender/messagesender.component'
 import msgBallonLeft from './msgBallon/msgBallonLeft.component'
 import msgBallonRight from './msgBallon/msgBallonRight.component'
+import glue from 'angularjs-scroll-glue'
 
-export default angular.module('messages',[])
+export default angular.module('messages',[glue])
     .component('messageBox',messageBox)
     .component('messageSender',messageSender)
     .component('msgBallonLeft',msgBallonLeft)
